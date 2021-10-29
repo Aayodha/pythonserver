eeb6abb21241ef4c7791d3d2ae3fe838446eb5b5
Aayodha <ayusharma0022@gmail.com>
2021-10-29 18:20:00 +0530

    Fixed

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from second import app
 
-
 if __name__ == '__main__':
     app.run(port=5500, debug = True)
--- second/routes.py
+++ second/routes.py
@@ -1,8 +1,9 @@
 from second import app
 from flask import render_template
 
-
 @app.route('/')
+def index():
+    return "Hello"
 @app.route('/home')
 def homepage():
     return render_template('homepage.html')
